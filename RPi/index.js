2a90906313b88c7d738398c06e9786cd84f52de3
Nick McGinness <ragnerd@gmail.com>
2017-09-20 22:56:38 -0500

    Got LCD and MPL3115A2 working for RPi

--- RPi/index.js
+++ RPi/index.js
@@ -7,21 +7,29 @@ const board = new five.Board({
 board.on('ready', () => {
 	const sensor = new five.Multi({ controller: 'MPL3115A2' });
 	const lcd = new five.LCD({
-		pins: [7, 8, 9, 10, 11, 12],
+		pins: ['GPIO7', 'GPIO8', 'GPIO9', 'GPIO10', 'GPIO11', 'GPIO12'],
 		backlight: 6,
 		rows: 2,
 		cols: 30
 	});
 
-	lcd.clear().print("Testing...");
+	lcd.clear().print('Testing...');
 	lcd.cursor(1, 0);
-	lcd.print("1... 2... 3!");
+	lcd.print('1... 2... 3!');
 
-	sensor.on('change', () => {
-		console.log('------');
-		console.log('Altitude: ', sensor.altimeter.feet);
-		console.log('Pressure: ', sensor.barometer.pressure);
-		console.log('Temperature: ', sensor.thermometer.F);
-		console.log('------');
+	function printAltitude() {
+		lcd.clear().print('Alt: ' + sensor.altimeter.feet);
+	}
+
+	function printEnvironment() {
+		lcd.clear().print('Pressure: ' + sensor.barometer.pressure);
+		lcd.cursor(1, 0);
+		lcd.print('Temp: ' + sensor.thermometer.F);
+	}
+
+	board.repl.inject({
+		lcd: lcd,
+		printAlt: printAltitude,
+		printEnv: printEnvironment
 	});
 });
